0a2423829b598f45ab2fe9c4439320a0ad1160b1
reas3 <reasivan2@gmail.com>
2024-09-03 13:07:30 +0300

    connect-db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const app = express();
-require('dotenv').config();
+const dotenv = require('dotenv')
+const {db} = require('./utils/db')
+dotenv.config()
+const app = require('./app')
+const connectionStr = process.env.MONGO_DB_ATLAS
+.replace('<password>', process.env.MONGO_DB_ATLAS_PASSWORD)
+.replace('<username>', process.env.MONGO_DB_ATLAS_USERNAME)
+console.log(connectionStr);
 
-// Connect Database
-connectDB();
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+db(connectionStr)
+const port = 8000 ||8001
+app.listen(port, '127.0.0.1', ()=>{
+    console.log(`Server is listening on port ${port}`);
+})
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,15 +1,12 @@
-// config/db.js
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-    });
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+const mongoose = require('mongoose')
+exports.db = async(conStr) => {
+    try{
+    const con = await mongoose.connect(conStr)
+if (con){
+    console.log(`the data base has been connected ${con.connection.host}`);
+}
+}
+    catch(err){
+        console.log(err.message);
+    }
+}
