546322ed30747c14a65af5169283a2b4f74c5bcf
netanelbi3 <netanelbi3@gmail.com>
2024-09-04 09:35:27 +0300

    build routes and side-bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MenuSideBar from './components/MenuSideBar';
+import Home from './pages/Home';
+import Inventory from "./pages/Inventory";
+import Transactions from "./pages/Transactions";
+import Users from "./pages/Users";
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+      <Router>
+        <div className="d-flex">
+          <MenuSideBar />
+          <div className="content-container p-4">
+            <Routes>
+              <Route path="/Home" element={<Home />} />
+              <Route path="/inventory" element={<Inventory />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/users" element={<Users />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    );
+  
 }
 
 export default App
